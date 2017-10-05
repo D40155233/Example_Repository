912f5af5bd71b46a5cf76f5e1edb042a6b18cf9a
Andrea Russomanno <31318298+D01057213@users.noreply.github.com>
2017-10-05 19:44:13 -0400

    Delete Andrea_add_file.sql

--- Andrea_add_file.sql
+++
@@ -1,5 +0,0 @@
-jkhhkljhlj
-Andrea Russomanno Sept 20 - Added file directly to local repository
-Added file directly to local repository - Jake Hartman
-now modifying file by adding this line
-testing2
